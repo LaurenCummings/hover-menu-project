3858127d2c6a547da64c2cc51e13479f52465d42
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-28 23:37:33 -0400

    Created global values and functions in context

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,2 +1,39 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, createContext } from 'react';
 import sublinks from './data';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+    [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+
+    const openSubmenu = () => {
+        setIsSubmenuOpen(true);
+    }
+
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    }
+
+    return (
+        <AppContext.Provider 
+            value={{
+                isSubmenuOpen, 
+                isSidebarOpen, 
+                openSubmenu, 
+                openSidebar, 
+                closeSubmenu, 
+                closeSidebar
+            }}>
+                {children}
+        </AppContext.Provider>
+    )
+}
