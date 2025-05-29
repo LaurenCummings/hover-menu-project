299e6a7063de2ee82bfa79e66a3ef0fa037ca051
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-28 23:16:05 -0400

    imported components into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import './App.css'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Sidebar from './components/Sidebar';
+import Submenu from './components/Submenu';
 
 function App() {
 
