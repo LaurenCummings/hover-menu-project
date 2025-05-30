a56c7abeb919070468dd26995186a5c72b2403e4
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-29 23:21:53 -0400

    added buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,31 @@ function Navbar() {
         <nav className="nav">
             <div className="nav-center">
                 <div className="nav-header">
-                    <img src={logo} alt="stripe" />
-
+                    <img src={logo} alt="stripe" className="nav-logo" />
+                    <button className="btn toggle-btn" onClick={openSidebar}>
+                        <FaBars />
+                    </button>
                 </div>
                 <ul className="nav-links">
-
+                    <li>
+                        <button className="link-btn">
+                            products
+                        </button>
+                    </li>
+                    <li>
+                        <button className="link-btn">
+                            developers
+                        </button>
+                    </li>
+                    <li>
+                        <button className="link-btn">
+                            company
+                        </button>
+                    </li>
                 </ul>
+                <button className="btn signin-btn">
+                    Sign in
+                </button>
             </div>
         </nav>
     )
