ae22abb8e14dfe5eb1ffab72fce4c311ebb433c5
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-01 23:00:25 -0400

    mapped through links and displayed icon and label for each one

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -16,6 +16,16 @@ function Submenu() {
     return (
         <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`} ref={container}>
             <h4>{page}</h4>
+            <div className={`submenu-center col-2`}>
+                {links.map((link, index) => {
+                    const { label, icon, url } = link;
+                    return (
+                        <a key={index} href={url}>
+                            {icon} {label}
+                        </a>
+                    )
+                })}
+            </div>
         </aside>
     )
 }
